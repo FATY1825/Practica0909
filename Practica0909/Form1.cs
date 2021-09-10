0018c31a47e9c1bc0ebff2f64415e5433f28de20
Fatima Ayala <ftayala89@gmail.com>
2021-09-09 22:03:44 -0600

    Actualizacion

--- Practica0909/Form1.cs
+++ Practica0909/Form1.cs
@@ -42,7 +42,39 @@ namespace Practica0909
 
         private void btnGetData_Click(object sender, EventArgs e)
         {
+            //agregar columnas al DataGridView
+            dgData.Columns.Add("idStudents", "CODIGO");
+            dgData.Columns.Add("firsname", "NOMBRE");
+            dgData.Columns.Add("lastname", "APELLIDO");
+            dgData.Columns.Add("age", "EDAD");
+            dgData.Columns.Add("nationality", "NACIONALIDAD");
 
+            foreach (var student in selectAllData())
+                {
+                dgData.Rows.Add();
+                }
+        }
+
+        private List<Student> selectAllData()
+        {
+            List<Student> query = (
+                from student in Student.GetStudents()
+                select new Student()
+                {
+                    idStudent = student.idStudent,
+                    fistName = student.fistName,
+                    lastName = student.lastName,
+                    age = student.age,
+                    nationality = student.nationality,
+                }
+                ).ToList();
+            return query;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        
         }
     }
+
 }
